deeb7632cce78e24c602401097489e92c27535ee
louiji25 <louiji.hubert@gmail.com>
2026-01-07 12:03:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def generate_thermal_ticket(type_doc, data, client_name, ref, options_txt):
     pdf.cell(72, 5, "LEMUR MACACO TOURS", ln=True, align='C')
     pdf.set_font("Helvetica", '', 7)
     pdf.cell(72, 4, "Nosy Be - Madagascar", ln=True, align='C')
-    pdf.cell(72, 4, "Contact: +261 32 00 000 00", ln=True, align='C')
+    pdf.cell(72, 4, "Contact: +261 32 26 393 88 / 34 29 010 65", ln=True, align='C')
     pdf.cell(72, 4, "NIF: 123456789 | STAT: 98765", ln=True, align='C')
     pdf.ln(2); pdf.cell(72, 0, "-"*45, ln=True, align='C'); pdf.ln(2)
     
@@ -225,6 +225,7 @@ with tab3:
     with c2:
         st.markdown("### 📗 Factures")
         st.dataframe(pd.read_csv(HIST_FACTURES), use_container_width=True)
+
 
 
 
